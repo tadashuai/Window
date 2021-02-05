a9f4ea7b8dcb1017f1ae98f69dd30db096b8e425
tadashuai <46532852+tadashuai@users.noreply.github.com>
2021-02-05 10:58:02 +0800

    Update ScreenshotMono.cs

--- Scripts/ScreenshotMono.cs
+++ Scripts/ScreenshotMono.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScreenshotMono : MonoBehaviour
 {
+	public Canvas canvas;
+	public GameObject imageGo;
+
 	void Update()
 	{
 		if ( Input.GetKeyDown( KeyCode.S ) )
@@ -12,19 +16,36 @@ public class ScreenshotMono : MonoBehaviour
 		}
 		if ( Input.GetKeyDown( KeyCode.C ) )
 		{
-			StartCoroutine( CaptureByRect() );
+			for ( int i = 0; i < 12; i++ )
+			{
+				StartCoroutine( CaptureByRect( i ) );
+			}
 		}
 	}
 
-	private IEnumerator CaptureByRect()
+	private IEnumerator CaptureByRect( int i )
 	{
 		yield return new WaitForEndOfFrame();
-		Texture2D mTexture = new Texture2D( Screen.width / 2, Screen.height / 2, TextureFormat.RGB24, false );
-		mTexture.ReadPixels( new Rect( Screen.width / 4, Screen.height / 4, Screen.width / 2, Screen.height / 2 ), 0, 0 );
+		Texture2D mTexture = new Texture2D( Screen.width / 4, Screen.height / 3, TextureFormat.RGB24, false );
+		mTexture.ReadPixels( new Rect( ( i % 4 ) * ( Screen.width / 4 ), ( i / 4 ) * ( Screen.height / 3 ), Screen.width / 4, Screen.height / 3 ), 0, 0 );
 		mTexture.Apply();
-		byte[] bytes = mTexture.EncodeToPNG();
-		System.IO.File.WriteAllBytes( Application.dataPath + "/Samples/ActionPreviewer/RectSc.png", bytes );
+		GameObject go = Instantiate( imageGo, canvas.transform );
+		go.name = "picture" + ( i % 4 ).ToString() + ( i / 4 ).ToString();
+		Image image = go.GetComponent<Image>();
+		RectTransform rt = image.GetComponent<RectTransform>();
+		rt.sizeDelta = new Vector2( Screen.width / 4, Screen.height / 3 );
+		rt.anchoredPosition = new Vector2( ( i % 4 ) * ( Screen.width / 4 ), ( i / 4 - 2 ) * ( Screen.height / 3 ) );
+		Sprite sprite = Sprite.Create( mTexture, new Rect( 0, 0, mTexture.width, mTexture.height ), Vector2.zero );
+		image.sprite = sprite;
 
-		UnityEditor.AssetDatabase.Refresh();
+		if ( i == 11 )
+			Destroy( imageGo );
+
+		//byte[] bytes = mTexture.EncodeToPNG();
+		//System.IO.File.WriteAllBytes( Application.dataPath + "/Samples/ActionPreviewer/RectSc.png", bytes );
+
+		//UnityEditor.AssetDatabase.Refresh();
+
+
 	}
 }
